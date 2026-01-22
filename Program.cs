29b7fead45d2bfb4ad5c001c448faef52e59e77d
Mosin-cmd <violetassasin2000@gmail.com>
2026-01-22 16:03:36 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
 //Proszę by każdy napisał jakiś algorytm sortowania. Jego nazwa to ma być jakiś totalnie losowy ciąg
+static int[] ADASDASDASDASDASDAS(int[] tab)
+{
+    bool swapped;
+    do
+    {
+        swapped = false;
+        for (int i = 1; i < tab.Length; i++)
+        {
+            if (tab[i - 1] > tab[i])
+            {
+                (tab[i - 1], tab[i]) = (tab[i], tab[i - 1]);
+                swapped = true;
+            }
+        }
+    } while (swapped);
+
+    return tab;
+}
