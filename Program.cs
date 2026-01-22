dd398e32e9e59da0b7c28356ed0287bb548689f8
MaoZedong112 <alexxxb200@gmail.com>
2026-01-22 13:04:08 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,3 +17,25 @@ static int[] ADASDASDASDASDASDAS(int[] tab)
 
     return tab;
 }
+
+static int[] jhszdjfaiufg(int[] array)
+{
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        int key = array[i];
+        int j = i - 1;
+
+        
+        while (j >= 0 && array[j] > key)
+        {
+            array[j + 1] = array[j];
+            j--;
+        }
+
+        
+        array[j + 1] = key;
+    }
+    return array;
+}
+
